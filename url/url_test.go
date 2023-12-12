05f68dcebeae0516fb9a3776536fd4c7d43df387
Leo Villalobos <villaleobos@gmail.com>
2023-12-12 12:37:30 -0800

    Implement tests for endpoints

--- url/url_test.go
+++ url/url_test.go
@@ -1,9 +1,60 @@
 package url
 
 import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestShortenAndFetch(t *testing.T) {
-	// TODO: Fix tests
+var (
+	testUrl = "https://www.villaleobos.com/"
+)
+
+func TestShorten(t *testing.T) {
+	ctx := context.Background()
+	params := ShortenParams{Url: testUrl}
+	url, err := Shorten(ctx, &params)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.NotNil(t, url)
+	assert.NotEmpty(t, url.Id)
+}
+
+func TestRemove(t *testing.T) {
+	ctx := context.Background()
+	params := ShortenParams{Url: testUrl}
+	// Add a URL to the database
+	url, err := Shorten(ctx, &params)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.NotNil(t, url)
+	// Remove the same URL
+	err = Remove(ctx, url.Id)
+	assert.Nil(t, err)
+}
+
+func TestFetch(t *testing.T) {
+	ctx := context.Background()
+	params := ShortenParams{Url: testUrl}
+	// Add a URL to the database
+	url, err := Shorten(ctx, &params)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.NotNil(t, url)
+	// Fetch the same URL
+	target := fmt.Sprintf("/url/%s", url.Id)
+	req := httptest.NewRequest(http.MethodGet, target, nil)
+	w := httptest.NewRecorder()
+	handler := http.HandlerFunc(FetchInternal)
+	handler.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusPermanentRedirect, w.Result().StatusCode)
+	location := w.Result().Header.Get("Location")
+	assert.Equal(t, url.Url, location)
 }
